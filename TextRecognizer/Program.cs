d29e4124e7e1d42868e8e4bf116163638d156fe7
BederKB <kostya.bkb@mail.ru>
2020-12-29 20:07:25 +0700

    init

---
+++ TextRecognizer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace TextRecognizer
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            // var img = Image.FromFile(@"C:\Users\Konstantin\Desktop\8.jpg");
+            //
+            // if (img == null) return;
+            //
+            // var cropped = CropImage(img, new Rectangle(0, 0, img.Width, img.Height / 2));
+            //
+            // var temp = Path.GetTempFileName();
+            // cropped.Save(temp);
+            //
+            // AsposeOcr api = new AsposeOcr();
+            //
+            // var result = api.RecognizeImage(temp);
+            // Console.WriteLine(result[0]);
+
+            var svc = new ReaderService(@"C:\Users\Konstantin\Desktop\cards");
+            await svc.StartRecognizing();
+        }
+    }
+}
---
+++ TextRecognizer/ReaderService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Aspose.OCR;
+
+namespace TextRecognizer
+{
+    public class ReaderService
+    {
+        private readonly string _path;
+        private readonly Channel<string> _queue;
+        private readonly AsposeOcr _ocr;
+        
+        public ReaderService(string path)
+        {
+            EnsurePath(path);
+            
+            _queue = Channel.CreateUnbounded<string>();
+            _path = path;
+            _ocr = new AsposeOcr();
+        }
+
+        public async Task StartRecognizing()
+        {
+            await AddFilesToQueue();
+
+            var tasks = Enumerable.Range(0, 10).Select(async _ => await RecognizeNext()).ToList();
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task AddFilesToQueue()
+        {
+            foreach (var file in Directory.GetFiles(_path))
+            {
+                Console.WriteLine($"add, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+                await _queue.Writer.WriteAsync(file);
+            }
+        }
+
+        private async Task RecognizeNext()
+        {
+            var file = await _queue.Reader.ReadAsync();
+
+            var img = Image.FromFile(file);
+            if (img == null) return;
+
+            var temp = Path.GetTempFileName();
+            CropHalfImage(img).Save(temp);
+
+            var result = _ocr.RecognizeImage(temp);
+            Console.WriteLine(result[0] + " Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static Image CropHalfImage(Image img)
+        {
+            var bmpImage = new Bitmap(img);
+            return bmpImage.Clone(new Rectangle(0, 0, img.Width, img.Height / 2), bmpImage.PixelFormat);
+        }
+
+        private static void EnsurePath(string path)
+        {
+            if (!Directory.Exists(path)) throw new Exception($"Folder: '{path}' doesn't exist.");
+        }
+    }
+}
